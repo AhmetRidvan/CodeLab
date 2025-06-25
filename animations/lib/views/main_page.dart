ef7d39efe9d99c0fcf64ff53b72c404a60314a5e
AhmetRidvan <ahmetridvanordulu@icloud.com>
2025-06-25 17:14:24 +0300

    wd
    wd

--- animations/lib/views/main_page.dart
+++ animations/lib/views/main_page.dart
@@ -5,6 +5,36 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final th = Theme.of(context).colorScheme;
+    return Scaffold(
+      backgroundColor: th.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              'images/vecteezy_3d-weather-icon-day-with-rain_24825193.png',
+              width: 300,
+            ),
+            Text(
+              'Weather',
+              style: TextStyle(fontSize: 36, color: th.onSurface),
+            ),
+            SizedBox(
+              width: 250,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: th.onSurface),
+                onPressed: () {},
+                child: Text(
+                  'Start',
+                  style: TextStyle(fontSize: 36, color: th.surface),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
